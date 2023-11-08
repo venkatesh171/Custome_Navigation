e1cd39588fa999dd11e34edba312ca62ea0235f4
venkatesh munaga <munagavenkatesh9@gmail.com>
2023-11-08 12:26:49 -0500

    styles
    installed a library called classnames
    used tailwind library
    added styles to buttons

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ export default function App () {
     return (
         <div>
             <div>
-            <Button primary outline>click here</Button>
-        </div>
-        <div>
-            <Button secondary rounded>Buy now</Button>
-        </div>
-        <div>
-            <Button danger outline>see deals</Button>
-        </div>
-        <div>
-            <Button warning>something</Button>
-        </div>
-        <div>
-            <Button success>children</Button>
-        </div>
+                <Button primary rounded outline>click here</Button>
+            </div>
+            <div>
+                <Button danger rounded>click here</Button>
+            </div>
+            <div>
+                <Button secondary rounded>Buy now</Button>
+            </div>
+            <div>
+                <Button danger outline>see deals</Button>
+            </div>
+            <div>
+                <Button warning>something</Button>
+            </div>
+            <div>
+                <Button success>children</Button>
+            </div>
         </div>
     );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import classnames from "classnames";
 
 
 export default function Button ({
@@ -11,7 +11,21 @@ export default function Button ({
     outline, 
     rounded
 }) {
-    return <button>{children}</button>;
+    const finalClassName = classnames("px-3 py-1.5 border", {
+        "border-blue-500 bg-blue-500 text-white": primary,
+        "border-gray-900 bg-gray-900 text-white": secondary,
+        "border-green-500 bg-green-500 text-white": success,
+        "border-yellow-500 bg-yellow-500 text-white": warning,
+        "border-red-500 bg-red-500 text-white": danger,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger
+    });
+    return <button className={finalClassName}>{children}</button>;
 }
 
 Button.propTypes = {
